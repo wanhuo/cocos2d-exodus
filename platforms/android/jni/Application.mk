7cf8284aaa7d0ed635bee0ae1015acbdbbb4b50a
Igor Mats <igor.mats@tooflya.com>
2015-10-08 15:44:22 +0300

    Update android platform.

--- platforms/android/jni/Application.mk
+++ platforms/android/jni/Application.mk
@@ -1,11 +1,9 @@
 APP_STL := gnustl_static
 
-APP_ABI := armeabi
-
-NDK_TOOLCHAIN_VERSION=clang
-
-APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
+APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -DCC_ENABLE_BULLET_INTEGRATION=1 -std=c++11 -fsigned-char
 APP_LDFLAGS := -latomic
+
+USE_ARM_MODE := 1
 
 ifeq ($(NDK_DEBUG),1)
   APP_CPPFLAGS += -DCOCOS2D_DEBUG=1
